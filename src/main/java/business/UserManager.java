5e869e34567d78730f1c782acd6c73adbf0a0ba5
Mercy <d00251752@student.dkit.ie>
2025-03-15 23:27:03 +0000

    SearchByName(connect username) method added in UserManager

---
+++ src/main/java/business/UserManager.java
@@ -0,0 +1,42 @@
+package business;
+
+import java.util.ArrayList;
+
+public class UserManager {
+    private void bootstrapUserList() {
+        User u1 = new User("John");
+        User u2 = new User("Sam");
+        User u3 = new User("Joanne");
+        User u4 = new User("Nathan");
+        User u5 = new User("Eva");
+        usersList.add(u1);
+        usersList.add(u2);
+        usersList.add(u3);
+        usersList.add(u4);
+        usersList.add(u5);
+    }
+
+    public UserManager() {
+        bootstrapUserList();
+    }
+
+    private final ArrayList<User> usersList = new ArrayList<User>();
+
+    /**
+     * Search user by username that store in usersList.
+     *
+     * @param username username of the user we want to search.
+     * @return a user by the specified username.
+     */
+    public User searchByUsername(String username) {
+        User user = null;
+        synchronized (usersList) {
+            for (User u : usersList) {
+                if (u.getUserName().equals(username)) {
+                    user = u;
+                }
+            }
+        }
+        return user;
+    }
+}
