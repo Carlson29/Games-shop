6d5974d4a57a8b35b097fe0dc5d4bbf976861331
Mercy <d00251752@student.dkit.ie>
2025-03-15 21:43:34 +0000

    Add GameService and test commit connected.

---
+++ src/main/java/business/GameService.java
@@ -0,0 +1,15 @@
+package business;
+
+public class GameService {
+
+    public static final String HOST = "localhost";
+    //not sure what port
+    public static final String REGISTER_REQUEST = "register";
+    public static final String SUCCESSFUL_REGISTRATION = "ADDED";
+    public static final String FAILED_REGISTRATION = "REJECTED";
+    public static final String LOGIN_REQUEST = "login";
+    public static final String SUCCESSFUL_USER_LOGIN = "SUCCESS_USER";
+    public static final String FAILED_LOGIN = "FAILED";
+
+
+}
