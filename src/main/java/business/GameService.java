3a829afefb6ede714a6d604b0da7b9eb1c718686
Mercy <d00251752@student.dkit.ie>
2025-03-15 22:11:14 +0000

    Format of requirement request and response added in GameService.

--- src/main/java/business/GameService.java
+++ src/main/java/business/GameService.java
@@ -4,12 +4,14 @@ public class GameService {
 
     public static final String HOST = "localhost";
     //not sure what port
-    public static final String REGISTER_REQUEST = "register";
-    public static final String SUCCESSFUL_REGISTRATION = "ADDED";
-    public static final String FAILED_REGISTRATION = "REJECTED";
-    public static final String LOGIN_REQUEST = "login";
-    public static final String SUCCESSFUL_USER_LOGIN = "SUCCESS_USER";
-    public static final String FAILED_LOGIN = "FAILED";
-
-
+    public static final String USER_REQUEST = "USER";
+    public static final String USER_CONNECT_RESPONSE = "CONNECTED";
+    public static final String ORDER_REQUEST = "ORDER";
+    public static final String ORDER_MATCH_RESPONSE = "MATCH";
+    public static final String CANCEL_REQUEST = "CANCEL";
+    public static final String CANCEL_CANCELLED_RESPONSE = "CANCELLED";
+    public static final String NOT_FOUND_RESPONSE = "NOT_FOUND";
+    public static final String VIEW_REQUEST = "VIEW";
+    public static final String END_REQUEST = "END";
+    public static final String END_RESPONSE = "ENDED";
 }
