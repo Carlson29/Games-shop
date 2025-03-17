848ee62bbedf3b9dc83e96bb94b96b8721d68302
Carlson29 <kometacarlson@gmail.com>
2025-03-17 20:00:07 +0000

    Added port and delimiter

--- src/main/java/business/GameService.java
+++ src/main/java/business/GameService.java
@@ -3,6 +3,7 @@ package business;
 public class GameService {
 
     public static final String HOST = "localhost";
+    public static final int PORT = 41238;
     //not sure what port
     public static final String USER_REQUEST = "USER";
     public static final String USER_CONNECT_RESPONSE = "CONNECTED";
@@ -14,6 +15,8 @@ public class GameService {
     public static final String VIEW_REQUEST = "VIEW";
     public static final String END_REQUEST = "END";
     public static final String END_RESPONSE = "ENDED";
-
+    public static final String DELIMITER = ",";
+    public static final String ACTION_DELIMITER = ":";
+    public static final String INVALID_REQUEST = "INVALID_REQUEST";
     //what to use for DELIMITER, we used %% for Michelle for film service
 }
